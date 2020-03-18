79251d0e729ffec49d9fba375c95d3b79847d273
Justin Montgomery <montgomerycodepro@gmail.com>
2020-03-18 13:21:27 -0400

    linking up more handlebars pages

--- controllers/handlebar-routes.js
+++ controllers/handlebar-routes.js
@@ -1,17 +1,21 @@
 //need to change html to handlebars.
 
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
 // var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
+    // if (req.user) {}
+    res.render("index");
+  });
+
+  app.get("/signup", (req, res) => {
     if (req.user) {
-      return res.redirect("/sportswappa");
+      res.render("index");
     }
     res.render("signup");
   });
@@ -22,16 +26,13 @@ module.exports = function(app) {
     }
     res.render("login");
   });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/sportswappa", isAuthenticated, function(req, res) {
-    res.render("index");
+  app.get("/sellItems", isAuthenticated, (req, res) => {
+    if (req.user) {
+      res.render("sell");
+    } else {
+      res.render("login");
+    }
   });
-
-  module.exports = function(app) {
-    app.get("/sportswapper", (req, res) => {
-      res.render("index");
-    });
-  };
 };
