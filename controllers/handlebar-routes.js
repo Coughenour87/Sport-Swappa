f7bb1e9a05bf3ea245bf582314e8eded42361e37
Julie Berryhill <juliesellsnc@gmail.com>
2020-03-20 11:10:01 -0400

    contact link

--- controllers/handlebar-routes.js
+++ controllers/handlebar-routes.js
@@ -75,7 +75,7 @@ module.exports = function(app) {
         console.log(err);
       });
   });
-  // app.get("/contact", (req,res)=> {
-  //   res.render("contact")
-  // })
+  app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
 };
